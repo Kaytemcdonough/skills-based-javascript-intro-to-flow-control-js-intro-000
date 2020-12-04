c76e67db37233c44c4e3718de7ae27d3e3f6990e
Kathryn McDonough <kaytemcdonough@gmail.com>
2020-12-04 18:03:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
 function basicTeenager(age) {
   if (age => 13 && age =<19) {
-    return "You are a teenager!";
+    console.log("You are a teenager!");
   }
 }
 
@@ -27,29 +27,5 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch(age) {
-    case 13:
-    return "You are a teenager"
-
-    case 14:
-    return "You are a teenager"
-
-    case 15:
-    return "You are a teenager"
-
-    case 16:
-    return "You are a teenager"
-
-    case 17:
-    return "You are a teenager"
-
-    case 18:
-    return "You are a teenager"
-
-    case 19:
-    return "You are a teenager"
-    default:
-    return "You have an age"}
-  }
 
 }
