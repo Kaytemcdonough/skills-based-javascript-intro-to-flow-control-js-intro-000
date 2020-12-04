242cbbb6850263e101271dd0466e7ff26936c26d
Kathryn McDonough <kaytemcdonough@gmail.com>
2020-12-04 17:51:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,11 +17,12 @@ function ageChecker(age) {
   else if (age <=12) {
     return "You are a kid";}
     else {return "You are a grownup";}
-  }
-
 }
 
 function ternaryTeenager(age) {
+  if (age =>13 && age =< 19) {
+    returtn "You are a teenager!";}
+    else {return "You are not a teenager";}
 
 }
 
